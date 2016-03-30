ef8402fdf93e49a5b7509a2c41f85fd71ba1e16d
SlaxXx <basti_12@gmx.de>
2016-03-30 23:52:26 +0200

    Added the swarm
    For the hive mind!

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entity : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(Random.Range(-1f,1f), Random.Range(-1f, 1f), 0f);
+       
+    }
+}
---
+++ Assets/Scripts/GameCreator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCreator : MonoBehaviour {
+
+    public int entityCount = 100;
+    public GameObject entity;
+    void Start()
+    {
+        for(int i = 0; i < entityCount; i++)
+        {
+            Instantiate(entity, new Vector3(Random.Range(-50f, 50f), Random.Range(-50f, 50f), 0f), Quaternion.identity);            
+        }
+        print ("Started");
+
+    }
+}
