79a5d5819434008d84f0f4a643cf40b73bb7b10a
B1gbug <big_bug7@msn.com>
2016-04-01 00:52:10 +0200

    Display...again
    angepasst und Basti-geprüft

--- Assets/Scripts/DisplayManager.cs
+++ Assets/Scripts/DisplayManager.cs
@@ -5,65 +5,59 @@ using System.Collections;
 public class DisplayManager : MonoBehaviour
 {
 
-    public float speed;
+
     public Text countgameObjects;
     float timeLastUpdate = 0f;
-    public float timeLastBreed = 0f;
-    private int count;
-    public int HighPower;
-    public int LowPower;
+
     public Text countText;
     public Text winText;
     public Text HPText;
     public Text LPText;
     public Text EText;
 
-    void Start ()
+    void Start()
     {
-        HighPower = 0;
-        LowPower = 0;
+
     }
 
     public GameObject[] evos;
 
     void Update()
     {
-        evos = GameObject.FindGameObjectsWithTag("Entity");
 
-        for (int i = 0; i < evos.Length; i++)
+
+
+    timeLastUpdate += Time.deltaTime;
+        if (timeLastUpdate >= 0.1f)
         {
-            
+            int highPower = 0;
+            int lowPower = 1000;
+            evos = GameObject.FindGameObjectsWithTag("Entity");
 
-            timeLastUpdate += Time.deltaTime;
-            if (timeLastUpdate >= 5f)
+            foreach (GameObject checkEntity in evos)
             {
-                foreach (GameObject checkEntity in evos)
+
+                int checkPower = checkEntity.GetComponent<Entity>().power;
+
+                if (checkPower > highPower)
                 {
-                
-                    int CheckPower = checkEntity.GetComponent<Entity>().power;
-
-                    if (CheckPower > HighPower)
-                    {
-                        HighPower = CheckPower;
-                    }
-                    if (CheckPower < LowPower)
-                    {
-                        LowPower = CheckPower;
-                    }
+                    highPower = checkPower;
                 }
-    
-                    countText.text = "Entities:         H. Power:          L. Power:    ";
-                HPText.text = "" + HighPower;
-                LPText.text = "" + LowPower;
-                EText.text = "" + i;
-
-                winText.text = "SlaMuBi Life Simulator";
-
-                //   print("Entities "+i+" highest power "+HighPower + " lowest power " + LowPower);
-                timeLastUpdate = 0f;
-                LowPower = 1000;
-                HighPower = 0;
+                if (checkPower < lowPower)
+                {
+                    lowPower = checkPower;
+                }
             }
+
+            countText.text = "Entities:         H. Power:          L. Power:    ";
+            HPText.text = "" + highPower;
+            LPText.text = "" + lowPower;
+            EText.text = "" + evos.Length;
+
+            winText.text = "SlaMuBi Life Simulator";
+
+            timeLastUpdate = 0f;
         }
+
     }
 }
