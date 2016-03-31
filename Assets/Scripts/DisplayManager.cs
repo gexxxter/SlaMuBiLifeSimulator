8fc84612dfe5e593654e148fed1907854848d62e
B1gbug <big_bug7@msn.com>
2016-04-01 00:20:58 +0200

    Display
    juhu wir sehen was...

---
+++ Assets/Scripts/DisplayManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DisplayManager : MonoBehaviour
+{
+
+    public float speed;
+    public Text countgameObjects;
+    float timeLastUpdate = 0f;
+    public float timeLastBreed = 0f;
+    private int count;
+    public int HighPower;
+    public int LowPower;
+    public Text countText;
+    public Text winText;
+    public Text HPText;
+    public Text LPText;
+    public Text EText;
+
+    void Start ()
+    {
+        HighPower = 0;
+        LowPower = 0;
+    }
+
+    public GameObject[] evos;
+
+    void Update()
+    {
+        evos = GameObject.FindGameObjectsWithTag("Entity");
+
+        for (int i = 0; i < evos.Length; i++)
+        {
+            
+
+            timeLastUpdate += Time.deltaTime;
+            if (timeLastUpdate >= 5f)
+            {
+                foreach (GameObject checkEntity in evos)
+                {
+                
+                    int CheckPower = checkEntity.GetComponent<Entity>().power;
+
+                    if (CheckPower > HighPower)
+                    {
+                        HighPower = CheckPower;
+                    }
+                    if (CheckPower < LowPower)
+                    {
+                        LowPower = CheckPower;
+                    }
+                }
+    
+                    countText.text = "Entities:         H. Power:          L. Power:    ";
+                HPText.text = "" + HighPower;
+                LPText.text = "" + LowPower;
+                EText.text = "" + i;
+
+                winText.text = "SlaMuBi Life Simulator";
+
+                //   print("Entities "+i+" highest power "+HighPower + " lowest power " + LowPower);
+                timeLastUpdate = 0f;
+                LowPower = 1000;
+                HighPower = 0;
+            }
+        }
+    }
+}
