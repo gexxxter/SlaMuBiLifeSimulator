877046f7f39efc4a0cc43cc192cfd7e257f646f2
SlaxXx <basti_12@gmx.de>
2016-03-31 20:23:31 +0200

    Added primitve collider

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -4,12 +4,26 @@ using System.Collections;
 public class Entity : MonoBehaviour {
 
     // Use this for initialization
+    float timeLastUpdate = 0f;
     void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position += new Vector3(Random.Range(-1f,1f), Random.Range(-1f, 1f), 0f);
+        timeLastUpdate += Time.deltaTime;
+        if(timeLastUpdate >= 0.033f)
+        {
+            transform.position += new Vector3(Random.Range(-1f,1f), Random.Range(-1f, 1f), 0f);
+            timeLastUpdate = 0f;
+        }
        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+       if(col.gameObject.tag == "Entity")
+        {
+            Destroy(col.gameObject);
+        }
     }
 }
