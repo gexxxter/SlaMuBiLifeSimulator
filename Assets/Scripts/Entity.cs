a36e4ee585e45a5dd15019dd6652811080fe9c24
SlaxXx <basti_12@gmx.de>
2016-03-31 21:50:45 +0200

    added stuff

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -4,11 +4,18 @@ using System.Collections;
 public class Entity : MonoBehaviour {
 
     public int power;
+    public int generation=0;
     // Use this for initialization
     float timeLastUpdate = 0f;
-    void Start () {
-        power = Random.Range(0, 1000);
+    public float timeLastBreed = 0f;
 
+
+    void Start () {
+        if(power != 0f)
+        {
+            return;
+        }
+        power = Random.Range(0, 1000);
     }
 	
 	// Update is called once per frame
@@ -19,7 +26,8 @@ public class Entity : MonoBehaviour {
             transform.position += new Vector3(Random.Range(-1f,1f), Random.Range(-1f, 1f), 0f);
             timeLastUpdate = 0f;
         }
-       
+        timeLastBreed += Time.deltaTime;
+
     }
 
     void OnCollisionEnter2D(Collision2D col)
@@ -29,7 +37,22 @@ public class Entity : MonoBehaviour {
             Entity enemy =col.gameObject.GetComponent<Entity>();
             if(enemy.power < this.power) {
                 Destroy(col.gameObject);
+                if(timeLastBreed > 0.000001f)
+                {
+                    GameObject newEntity=clone(col.gameObject.transform.position);
+                    newEntity.GetComponent<Entity>().power = power;
+                    timeLastBreed = 0f;
+                }
             }
         }
+    }
+
+    private GameObject clone(Vector3 position)
+    {
+        GameObject newEntity = (GameObject)Instantiate(this.gameObject, position, Quaternion.identity);
+        newEntity.GetComponent<Entity>().power = this.power;
+        newEntity.GetComponent<Entity>().generation = this.generation + 1;
+        
+        return newEntity;
     }
 }
